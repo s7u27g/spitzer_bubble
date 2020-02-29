ab635193815000029a6b587159e4873d3ee8839a
s7u27g <s7u27astr0@gmail.com>
2020-02-29 16:34:57 +0000

    bug fix #1

--- src/utils/data_utils.py
+++ src/utils/data_utils.py
@@ -1,10 +1,12 @@
+import pathlib
 import urllib.request
 
-def download_mipsgal_data(l, b, save_path='/root/data/science/spitzer/mipsgal/'):
+def download_mipsgal_data(l, b, save_path):
     '''
-    l must be int.
+    l must be int from 0 to 359.
     b must be str, 'p' or 'n'.
     '''
+    save_path = pathlib.Path(save_path).expanduser()
     url = 'https://irsa.ipac.caltech.edu/data/SPITZER/MIPSGAL/images/mosaics24/'
     l = str(l)+'0'
     while len(l)!=4:
@@ -12,8 +14,7 @@ def download_mipsgal_data(l, b, save_path='/root/data/science/spitzer/mipsgal/')
         
     file_name = 'MG'+l+b+'005_024.fits'
     with urllib.request.urlopen(url+file_name) as u:
-        with open(save_path+file_name, 'bw') as o:
+        with open(save_path/file_name, 'bw') as o:
             o.write(u.read())
             
     return
-
