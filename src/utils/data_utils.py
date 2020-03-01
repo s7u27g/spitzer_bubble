a2b2210781e4c5bf11a0b72b133fc801cc3b76a4
s7u27g <s7u27astr0@gmail.com>
2020-03-01 06:59:56 +0000

    add make_download_sage #5

--- src/utils/data_utils.py
+++ src/utils/data_utils.py
@@ -53,3 +53,30 @@ def download_glimpse_data(l, band, save_path):
             o.write(u.read())
             
     return
+
+def download_sage_data(band, save_path):
+    '''
+    band must be str, irac1, 2, 3, 4 or mips24.
+    '''
+    instrument_dict = {
+        'irac': 'irac/v2.2_8x8_2.0_images/',
+        'mips': 'mips_full_mos/',
+    }
+    
+    band_dict = {
+        'irac1': 'SAGE_LMC_IRAC3.6_2_mosaic.fits',
+        'irac2': 'SAGE_LMC_IRAC4.5_2_mosaic.fits',
+        'irac3': 'SAGE_LMC_IRAC5.8_2_mosaic.fits',
+        'irac4': 'SAGE_LMC_IRAC8.0_2_mosaic.fits',
+        'mips24': 'SAGE_LMC_MIPS24_E2.fits',
+    }
+    save_path = pathlib.Path(save_path).expanduser()
+    url = 'https://irsa.ipac.caltech.edu//data/SPITZER/SAGE/images/'
+    url += instrument_dict[band[:4]]
+    
+    file_name = band_dict[band]
+    with urllib.request.urlopen(url+file_name) as u:
+        with open(save_path/file_name, 'bw') as o:
+            o.write(u.read())
+            
+    return
