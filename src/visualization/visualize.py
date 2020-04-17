e1cd0029921e063eaa7b752edab4c41d2e43e38c
s7u27g <s7u27astr0@gmail.com>
2020-04-17 02:11:46 +0000

    add data_view

--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -4,15 +4,15 @@ from PIL import Image, ImageDraw
 def data_view(col, imgs, infos=None):
     '''
     col: number of columns
-    imgs: tensor or nparray with a shape of (?, y, x, 3)
+    imgs: tensor or nparray with a shape of (?, y, x, 1) or (?, y, x, 3)
     infos: dictonary from CutTable
     '''
+    imgs = np.uint8(imgs[:, :, :, 0]) if imgs.shape[3] == 1 else np.uint8(imgs)
     row = (lambda x, y: x//y if x/y-x//y==0.0 else x//y+1)(imgs.shape[0], col)
     dst = Image.new('RGB', (imgs.shape[1]*col, imgs.shape[2]*row))
 
     for i, arr in enumerate(imgs):
-        type(imgs)
-        img = Image.fromarray(np.uint8(arr))
+        img = Image.fromarray(arr)
         if infos != None:
             draw = ImageDraw.Draw(img)
             draw.text((3,3),infos[i]['name'])
