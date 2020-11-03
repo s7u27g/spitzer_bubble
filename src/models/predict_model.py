7bf81318b05718d48195d139cd05f2cf8cc4e3bf
s7u27g <s7u27astr0@gmail.com>
2020-11-03 07:16:38 +0000

    add func in src

---
+++ src/models/predict_model.py
@@ -0,0 +1,326 @@
+import time
+import tqdm
+import numpy
+import tensorflow
+from ..utils.processing import *
+
+def calc_yx_num(cut_shape, slide_pix, map_shape):
+    y_num = (int(map_shape[0])-cut_shape[0])//slide_pix[0] + 1
+    x_num = (int(map_shape[1])-cut_shape[1])//slide_pix[1] + 1
+#     y_num = (map_shape[0]-cut_shape[0])//slide_pix[0] + 1
+#     x_num = (map_shape[1]-cut_shape[1])//slide_pix[1] + 1
+    return y_num, x_num
+
+def get_indices(d_idx, y_num, x_num):
+    '''
+    just calculate indices
+    return: index array
+    '''
+    y = numpy.arange(0, y_num*d_idx[0], d_idx[0])
+    x = numpy.arange(0, x_num*d_idx[1], d_idx[1])
+    y_idx = (y[:,None]*1 + x[None,:]*0).ravel()[:,None]
+    x_idx = (y[:,None]*0 + x[None,:]*1).ravel()[:,None]
+#    x_idx, y_idx = numpy.meshgrid(x, y)
+    indices = numpy.concatenate([
+        y_idx.ravel()[:,None],
+        x_idx.ravel()[:,None],
+    ], axis=1)
+    return indices
+
+def calc_resize_num(cut_shape, ch_num, dtype, mem_size):
+    bytes_ = {'float32': 4, 'float64': 8}
+    resize_num = int((mem_size*10**9)/(cut_shape[0]*cut_shape[1]*ch_num*bytes_[dtype]))
+    return resize_num
+
+def calc_loop_num(all_num, part_num):
+    loop_num = all_num//part_num
+    if (all_num)%part_num==0: pass
+    else: loop_num += 1
+    return loop_num
+
+def clip_data_st(data, st_idx, cut_shape):
+    clip_data = []
+    for i in st_idx:
+        d = data[
+            i[0]:i[0]+cut_shape[0],
+            i[1]:i[1]+cut_shape[1]
+        ][None]
+        clip_data.append(d)
+        pass
+    clip_data = numpy.concatenate(clip_data)
+    return clip_data
+    
+def inference(model, data, inf_num):
+    inf_loop = calc_loop_num(len(data), inf_num)
+    prob = []
+    for i in range(inf_loop):
+        d = data[i*inf_num:(i+1)*inf_num]
+        p = model(d).numpy()[:, 1].tolist()
+        prob += p
+        pass
+    return prob
+
+
+def calc_prob2(model, data, cut_shape):
+    '''
+    model: keras model object
+    data: arr that is shape must be (y, x, color) or (y, x)
+    cut_shape: tuple or list
+    '''
+    input_shape = model.input_shape[1:3]
+    if data.ndim == 2: data = data[:,:,None]
+    else: pass
+    
+    if input_shape[0]*input_shape[1]>cut_shape[0]*cut_shape[1]:
+        y_size = input_shape[0]
+        x_size = input_shape[1]
+        pass
+    else:
+        y_size = cut_shape[0]
+        x_size = cut_shape[1]
+        pass
+
+    slide_pix = (int(round(cut_shape[0]/10)), int(round(cut_shape[1]/10)))
+    map_shape = data.shape[:2]
+    ch_num = data.shape[2]
+    dtype = data.dtype.name
+
+    yx_num = calc_yx_num(cut_shape, slide_pix, map_shape)
+    st_idx = get_indices(slide_pix, *yx_num)
+    resize_num = calc_resize_num((y_size, x_size), ch_num, dtype, 0.2)
+    resize_loop = calc_loop_num(yx_num[0]*yx_num[1], resize_num)
+    
+    inf_num = 2048
+    prob = []
+    for i in tqdm.tqdm(range(resize_loop)):
+        _st_idx = st_idx[i*resize_num:(i+1)*resize_num]
+        d = clip_data_st(data, _st_idx, cut_shape)
+        d = tensorflow.convert_to_tensor(d)
+        d = resize(d, input_shape)
+        d = standardize(d)
+        prob += inference(model, d, inf_num)
+        pass
+    
+    info = {
+        'y_num': yx_num[0], 'x_num': yx_num[1],
+        'y_cut': cut_shape[0], 'x_cut': cut_shape[1],
+        'y_sld': slide_pix[0], 'x_sld': slide_pix[1],
+        'y_org': data.shape[0], 'x_org': data.shape[1],
+    }
+    prob = numpy.array(prob)
+    
+    return info, prob
+
+def calc_prob3(model, data, cut_shape, sld_fac):
+    '''
+    model: keras model object
+    data: arr that is shape must be (y, x, color) or (y, x)
+    cut_shape: tuple or list
+    '''
+    input_shape = model.input_shape[1:3]
+    if data.ndim == 2: data = data[:,:,None]
+    else: pass
+    
+    if input_shape[0]*input_shape[1]>cut_shape[0]*cut_shape[1]:
+        y_size = input_shape[0]
+        x_size = input_shape[1]
+        pass
+    else:
+        y_size = cut_shape[0]
+        x_size = cut_shape[1]
+        pass
+
+    slide_pix = (int(round(cut_shape[0]/sld_fac)), int(round(cut_shape[1]/sld_fac)))
+    map_shape = data.shape[:2]
+    ch_num = data.shape[2]
+    dtype = data.dtype.name
+
+    yx_num = calc_yx_num(cut_shape, slide_pix, map_shape)
+    st_idx = get_indices(slide_pix, *yx_num)
+    resize_num = calc_resize_num((y_size, x_size), ch_num, dtype, 0.2)
+    resize_loop = calc_loop_num(yx_num[0]*yx_num[1], resize_num)
+    
+    inf_num = 2048
+    prob = []
+    for i in tqdm.tqdm(range(resize_loop)):
+        _st_idx = st_idx[i*resize_num:(i+1)*resize_num]
+        d = clip_data_st(data, _st_idx, cut_shape)
+        d = tensorflow.convert_to_tensor(d)
+        d = resize(d, input_shape)
+        d = standardize(d)
+        prob += inference(model, d, inf_num)
+        pass
+    
+    info = {
+        'y_num': yx_num[0], 'x_num': yx_num[1],
+        'y_cut': cut_shape[0], 'x_cut': cut_shape[1],
+        'y_sld': slide_pix[0], 'x_sld': slide_pix[1],
+        'y_org': data.shape[0], 'x_org': data.shape[1],
+    }
+    prob = numpy.array(prob)
+    
+    return info, prob
+
+
+
+
+# def calc_prob(model, data, cut_shape):
+#     '''
+#     model: keras model object
+#     data: arr that is shape must be (y, x, color) or (y, x)
+#     cut_shape: tuple or list
+#     '''
+#     input_shape = model.input_shape[1:3]
+#     if data.ndim == 2: data = data[:,:,None]
+#     else: pass
+    
+#     if input_shape[0]*input_shape[1]>cut_shape[0]*cut_shape[1]:
+#         y_size = input_shape[0]
+#         x_size = input_shape[1]
+#         pass
+#     else:
+#         y_size = cut_shape[0]
+#         x_size = cut_shape[1]
+#         pass
+
+#     slide_pix = (cut_shape[0]//10, cut_shape[1]//10)
+#     map_shape = data.shape[:2]
+#     ch_num = data.shape[2]
+#     dtype = data.dtype.name
+
+#     yx_num = calc_yx_num(cut_shape, slide_pix, map_shape)
+#     st_idx = get_indices(slide_pix, *yx_num)
+#     resize_num = calc_resize_num((y_size, x_size), ch_num, dtype, 0.2)
+#     resize_loop = calc_loop_num(yx_num[0]*yx_num[1], resize_num)
+    
+#     inf_num = 2048
+#     prob = []
+#     for i in tqdm.tqdm(range(resize_loop)):
+#         _st_idx = st_idx[i*resize_num:(i+1)*resize_num]
+#         d = clip_data_st(data, _st_idx, cut_shape)
+#         d = tensorflow.convert_to_tensor(d)
+#         #d = resize(d, (100, 100))
+#         #d = remove_star(d, (3,3))
+#         d = resize(d, input_shape)
+#         d = standardize(d)
+#         prob += inference(model, d, inf_num)
+#         pass
+    
+#     info = {
+#         'y_num': yx_num[0], 'x_num': yx_num[1],
+#         'y_cut': cut_shape[0], 'x_cut': cut_shape[1],
+#         'y_sld': slide_pix[0], 'x_sld': slide_pix[1],
+#         'y_org': data.shape[0], 'x_org': data.shape[1],
+#     }
+#     prob = numpy.array(prob)
+    
+#     return info, prob
+
+
+
+# def _calc_prob(model, data, cut_shape):
+#     input_shape = model.input_shape[1:3]
+#     if data.ndim == 2: data = data[:,:,None]
+#     else: pass
+    
+#     if input_shape[0]*input_shape[1]>cut_shape[0]*cut_shape[1]:
+#         y_size = input_shape[0]
+#         x_size = input_shape[1]
+#         pass
+#     else:
+#         y_size = cut_shape[0]
+#         x_size = cut_shape[1]
+#         pass
+
+#     slide_pix = (cut_shape[0]//10, cut_shape[1]//10)
+#     map_shape = data.shape[:2]
+#     ch_num = data.shape[2]
+#     dtype = data.dtype.name
+
+#     yx_num = calc_yx_num(cut_shape, slide_pix, map_shape)
+#     st_idx = get_indices(slide_pix, *yx_num)
+#     resize_num = calc_resize_num((y_size, x_size), ch_num, dtype, 0.8)
+#     resize_loop = calc_loop_num(yx_num[0]*yx_num[1], resize_num)
+#     print(resize_loop)
+    
+#     inf_num = 2048
+#     prob = []
+#     for i in tqdm.tqdm(range(resize_loop)):
+#         _st_idx = st_idx[i*resize_num:(i+1)*resize_num]
+#         d = clip_data_st(data, _st_idx, cut_shape)
+#         d = resize(d, input_shape)
+#         d = standardize(d)
+#         prob += inference(model, d, inf_num)
+#         pass
+    
+#     info = {
+#         'y_num': yx_num[0], 'x_num': yx_num[1],
+#         'y_cut': cut_shape[0], 'x_cut': cut_shape[1],
+#         'y_sld': slide_pix[0], 'x_sld': slide_pix[1],
+#         'y_org': data.shape[0], 'x_org': data.shape[1],
+#     }
+#     prob = numpy.array(prob)
+    
+#     return info, prob
+
+
+
+# def _calc_prob(model, data, cut_shape):
+#     input_shape = model.input_shape[1:3]
+#     if data.ndim == 2: data = data[:,:,None]
+#     else: pass
+    
+#     if input_shape[0]*input_shape[1]>cut_shape[0]*cut_shape[1]:
+#         y_size = input_shape[0]
+#         x_size = input_shape[1]
+#         pass
+#     else:
+#         y_size = cut_shape[0]
+#         x_size = cut_shape[1]
+#         pass
+
+#     slide_pix = (cut_shape[0]//10, cut_shape[1]//10)
+#     map_shape = data.shape[:2]
+# #     if len(data.shape) == 2: ch_num = 1
+# #     else: ch_num = data.shape[2]
+#     ch_num = data.shape[2]
+#     dtype = data.dtype.name
+
+#     yx_num = calc_yx_num(cut_shape, slide_pix, map_shape)
+#     st_idx = get_indices(slide_pix, *yx_num)
+#     resize_num = calc_resize_num((y_size, x_size), ch_num, dtype, 0.8)
+#     resize_loop = calc_loop_num(yx_num[0]*yx_num[1], resize_num)
+#     print(resize_loop)
+    
+#     inf_num = 2048
+#     prob = []
+#     t0 = 0
+#     t1 = 0
+#     t2 = 0
+#     t3 = 0
+#     for i in tqdm.tqdm(range(10)):
+#         _st_idx = st_idx[i*resize_num:(i+1)*resize_num]
+#         t = time.time()
+#         d = clip_data_st(data, _st_idx, cut_shape)
+#         t0 += t-time.time()
+#         t = time.time()
+#         d = resize(d, input_shape)
+#         t1 += t-time.time()
+#         t = time.time()
+#         d = standardize(d)
+#         t2 += t-time.time()
+#         t = time.time()
+#         prob += inference(model, d, inf_num)
+#         t3 += t-time.time()
+#         pass
+    
+#     info = {
+#         'y_num': yx_num[0], 'x_num': yx_num[1],
+#         'y_cut': cut_shape[0], 'x_cut': cut_shape[1],
+#         'y_sld': slide_pix[0], 'x_sld': slide_pix[1],
+#         'y_org': data.shape[0], 'x_org': data.shape[1],
+#     }
+#     prob = numpy.array(prob)
+    
+#     print(t0, t1, t2, t3)
+#     return info, prob
---
+++ src/utils/get_catalog.py
@@ -0,0 +1,85 @@
+import astroquery.vizier
+import pandas as pd
+
+def churchwell_bubble():
+    # make instance
+    viz = astroquery.vizier.Vizier(columns=['*'])
+    viz.ROW_LIMIT = -1
+    # load bub_2006
+    bub_2006 = viz.query_constraints(catalog='J/ApJ/649/759/bubbles')[0].to_pandas()
+    bub_2006.loc[:, '__CPA2006_'] = bub_2006.loc[:, '__CPA2006_'].str.decode('utf-8')
+    bub_2006.loc[:, 'MFlags'] = bub_2006.loc[:, 'MFlags'].str.decode('utf-8')
+    # load bub_2007
+    bub_2007 = viz.query_constraints(catalog='J/ApJ/670/428/bubble')[0].to_pandas()
+    bub_2007.loc[:, '__CWP2007_'] = bub_2007.loc[:, '__CWP2007_'].str.decode('utf-8')
+    bub_2007.loc[:, 'MFlags'] = bub_2007.loc[:, 'MFlags'].str.decode('utf-8')
+    # convert to pandas for 2006
+    bub_2006.rename(columns={'__CPA2006_': 'name'}, inplace=True)
+    bub_2006.rename(columns={'GLON': 'l'}, inplace=True)
+    bub_2006.rename(columns={'GLAT': 'b'}, inplace=True)
+    bub_2006.rename(columns={'__R_': '<R>'}, inplace=True)
+    bub_2006.rename(columns={'__T_': '<T>'}, inplace=True)
+    bub_2006.rename(columns={'MFlags': 'Flags'}, inplace=True)
+    bub_2006.rename(columns={'_RA.icrs': 'RA.icrs'}, inplace=True)
+    bub_2006.rename(columns={'_DE.icrs': 'DE.icrs'}, inplace=True)
+    bub_2006 = bub_2006.set_index('name')
+    # convert to pandas for 2007
+    bub_2007.rename(columns={'__CWP2007_': 'name'}, inplace=True)
+    bub_2007.rename(columns={'GLON': 'l'}, inplace=True)
+    bub_2007.rename(columns={'GLAT': 'b'}, inplace=True)
+    bub_2007.rename(columns={'__R_': '<R>'}, inplace=True)
+    bub_2007.rename(columns={'__T_': '<T>'}, inplace=True)
+    bub_2007.rename(columns={'MFlags': 'Flags'}, inplace=True)
+    bub_2007.rename(columns={'_RA.icrs': 'RA.icrs'}, inplace=True)
+    bub_2007.rename(columns={'_DE.icrs': 'DE.icrs'}, inplace=True)
+    for i in bub_2007.index:
+        bub_2007.loc[i, 'name'] = bub_2007.loc[i, 'name'].replace(' ', '')
+        pass
+    bub_2007 = bub_2007.set_index('name')
+    # concat 2006 and 2007
+    bub = pd.concat([bub_2006, bub_2007])
+    return bub
+
+def mwp_bubble():
+    viz = astroquery.vizier.Vizier(columns=['*'])
+    viz.ROW_LIMIT = -1
+    mwp_small = viz.query_constraints(catalog='J/MNRAS/424/2442/mwpsmall')[0].to_pandas()
+    mwp_small.loc[:, 'MWP'] = mwp_small.loc[:, 'MWP'].str.decode('utf-8')
+    mwp_small.loc[:, 'ONames'] = mwp_small.loc[:, 'ONames'].str.decode('utf-8')
+    mwp_small.loc[:, 'Simbad'] = mwp_small.loc[:, 'Simbad'].str.decode('utf-8')
+    mwp_small = mwp_small.drop(columns='Flag')
+    #mwp_small.loc[:, 'Flag'] = mwp_small.loc[:, 'Flag'].str.decode('utf-8')
+    mwp_large = viz.query_constraints(catalog='J/MNRAS/424/2442/mwplarge')[0].to_pandas()
+    mwp_large.loc[:, 'MWP'] = mwp_large.loc[:, 'MWP'].str.decode('utf-8')
+    mwp_large.loc[:, 'ONames'] = mwp_large.loc[:, 'ONames'].str.decode('utf-8')
+    mwp_large.loc[:, 'Simbad'] = mwp_large.loc[:, 'Simbad'].str.decode('utf-8')
+    mwp_large = mwp_large.drop(columns='Flag')
+    #mwp_large.loc[:, 'Flag'] = mwp_large.loc[:, 'Flag'].str.decode('utf-8')
+    mwp_small.rename(columns={'MWP': 'name'}, inplace=True)
+    mwp_small.rename(columns={'GLON': 'l'}, inplace=True)
+    mwp_small.rename(columns={'GLAT': 'b'}, inplace=True)
+    mwp_small.rename(columns={'Reff': 'Rout'}, inplace=True)
+    mwp_large.rename(columns={'MWP': 'name'}, inplace=True)
+    mwp_large.rename(columns={'GLON': 'l'}, inplace=True)
+    mwp_large.rename(columns={'GLAT': 'b'}, inplace=True)
+    mwp_large.rename(columns={'Reff': 'Rout'}, inplace=True)
+    bub = pd.concat([mwp_small, mwp_large], axis=0, sort=False).set_index('name')
+    return bub
+
+def wise_hii():
+    viz = astroquery.vizier.Vizier(columns=['*'])
+    viz.ROW_LIMIT = -1
+    df = viz.query_constraints(catalog='J/ApJS/212/1/wisecat')[0].to_pandas()
+    df.loc[:, 'WISE'] = df.loc[:, 'WISE'].str.decode('utf-8')
+    df.loc[:, 'Cl'] = df.loc[:, 'Cl'].str.decode('utf-8')
+    df.loc[:, 'Ref'] = df.loc[:, 'Ref'].str.decode('utf-8')
+    df.loc[:, 'Name'] = df.loc[:, 'Name'].str.decode('utf-8')
+    df.loc[:, 'Mol'] = df.loc[:, 'Mol'].str.decode('utf-8')
+    df.rename(columns={'WISE': 'name'}, inplace=True)
+    df.rename(columns={'GLON': 'l'}, inplace=True)
+    df.rename(columns={'GLAT': 'b'}, inplace=True)
+    df.rename(columns={'Rad': 'Rout'}, inplace=True)
+    df.rename(columns={'Name': 'Hii_name'}, inplace=True)
+    df = df.set_index('name')
+    return df
+
