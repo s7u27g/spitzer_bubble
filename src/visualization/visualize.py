2706d445a94b962c316c0837ad587fde5903c878
s7u27g <s7u27astr0@gmail.com>
2021-03-01 13:40:54 +0900

    add 3 functions for tile view

--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -2,8 +2,7 @@ import numpy as np
 from PIL import Image, ImageDraw, ImageOps
 import matplotlib.patches
 from astropy.units import arcmin, deg
-
-
+from ..utils import processing
 
 def data_view(col, imgs, infos=None):
     '''
@@ -25,25 +24,29 @@ def data_view(col, imgs, infos=None):
 
     return dst
 
-def data_view2(col, imgs, infos=None):
-    '''
-    col: number of columns
-    imgs: tensor or nparray with a shape of (?, y, x, 1) or (?, y, x, 3)
-    infos: dictonary from CutTable
-    '''
-    imgs = np.uint8(imgs[:,:,:,0]) if imgs.shape[3] == 1 else np.uint8(imgs[:,:])
-    row = (lambda x, y: x//y if x/y-x//y==0.0 else x//y+1)(imgs.shape[0], col)
-    dst = Image.new('RGB', (imgs.shape[1]*col, imgs.shape[2]*row))
+def processing_func(data, norm_func=None, shape=(50, 50)):
+    shape = (shape[0]*2, shape[1]*2)
+    data = processing.smoothing(data, shape)
+    tensor = tf.convert_to_tensor(data)
+    tensor = processing.resize(tensor, shape)
+    tensor = processing.crop(tensor, 1/2)
+    if norm_func is not None:
+        tensor = norm_func(tensor)
+        pass
+    data = tensor.numpy()
+    return data
 
-    for i, arr in enumerate(imgs):
-        img = Image.fromarray(arr)
-        if infos != None:
-            draw = ImageDraw.Draw(img)
-            draw.text((3,3),infos[i]['name'])
-        quo, rem = i//col, i%col
-        dst.paste(img, (arr.shape[0]*rem, arr.shape[1]*quo))
+def scale255_for_standerlize(data_cube, bias=4, factor=20):
+    data_cube = (data_cube+bias)*factor
+    data_cube[data_cube>255] = 255
+    data_cube[data_cube<0] = 0
+    data_cube = np.uint8(data_cube)
+    return data_cube
 
-    return ImageOps.flip(dst)
+def scale255_for_normalize(data_cube):
+    data_cube = data_cube*255
+    data_cube = np.uint8(data_cube)
+    return data_cube
 
 def _draw_circle(ax, info, linewitdh, edgecolor):
     ax.add_patch(
