263fd7f80c171e0f840960b4d7f124b966bcc15c
s7u27g <s7u27astr0@gmail.com>
2021-11-20 14:43:59 +0900

    fix func

--- src/tmp_func/tmp_func.py
+++ src/tmp_func/tmp_func.py
@@ -173,14 +173,17 @@ def _open_json(file):
         pass
     return infos
 
-def json2reg(json_path, shape='circle'):
+def json2reg(json_path, shape='circle', R_unit=None):
     import simplejson as json
     json_path = path_formatter(json_path)
     save_path = json_path.parent.parent/'reg'/(json_path.stem+'.reg')
     df = pd.DataFrame(_open_json(json_path))
     infos = df.to_dict('records')
     if shape=='circle':
-        df['R'] *= 60
+        if R_unit: 'deg': df['R'] *= 60
+        elif R_unit: 'arcmin': pass
+        elif R_unit: 'arcsec': df['R'] /= 60       
+        else: pass
         make_regfile_cir(infos=infos, file=save_path, coord='fk5')
         pass
     elif shape=='point':
